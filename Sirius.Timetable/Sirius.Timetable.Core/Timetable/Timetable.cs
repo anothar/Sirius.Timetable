b51e19ca7fcc7eee7731a0c9a8092a1b34e670c4
code-creater <rino2000@yandex.ru>
2017-02-11 17:10:00 +0300

    Add Core files

---
+++ Sirius.Timetable/Sirius.Timetable.Core/Timetable/Timetable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sirius.Timetable.Core
+{
+    /// <summary>
+    /// Расписание на один день для всех групп
+    /// </summary>
+	public class Timetable
+	{
+        /// <summary>
+        /// Словарь расписаний для каждой команды
+        /// </summary>
+		public Dictionary<string, List<Activity>> Teams { get; set; }
+	}
+}
---
+++ Sirius.Timetable/Sirius.Timetable.Core/Timetable/Utilites/Activity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sirius.Timetable.Core
+{
+    /// <summary>
+    /// Событие дня для одной группы
+    /// </summary>
+	public class Activity
+	{
+        /// <summary>
+        /// Время начала события
+        /// </summary>
+		public DateTime? Start { get; set; }
+        /// <summary>
+        /// Время конца события, null - если нет
+        /// </summary>
+		public DateTime? End { get; set; }
+        /// <summary>
+        /// Название и описание события
+        /// </summary>
+		public string Title { get; set; }
+        /// <summary>
+        /// Место события
+        /// </summary>
+		public string Place { get; set; }
+        /// <summary>
+        /// Время отъезда автобуса на мероприятие, null - если нет автобуса
+        /// </summary>
+		public DateTime? BusTo { get; set; }
+        /// <summary>
+        /// Время отъезда автобуса с мероприятия, null - если нет автобуса
+        /// </summary>
+		public DateTime? BusFrom { get; set; }
+	}
+}
